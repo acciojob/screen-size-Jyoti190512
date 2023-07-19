6788f2e19fafdb3dd5c4bf4b3104336c3373451d
Jyoti190512 <134208281+Jyoti190512@users.noreply.github.com>
2023-07-20 00:17:38 +0100

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-//your JS code here. If required.
- let height=this.innerHeight;
-    let width=this.innerWidth;
-   let div=document.getElementById("sizeInfo");
-    let h=div.firstChild;
-    h.innerText="Width: "+width+" and Height: "+height;  
-window.addEventListener("resize",function(){
-    let height=this.innerHeight;
-    let width=this.innerWidth;
-   let div=document.getElementById("sizeInfo");
-    let h=div.firstChild;
- 
-    h.innerText="Width: "+width+" and Height: "+height; 
-  
-  })
+// Get references to the <h1> tags
+const widthTag = document.querySelector("#sizeInfo h1:nth-of-type(1)");
+const heightTag = document.querySelector("#sizeInfo h1:nth-of-type(2)");
+
+// Function to update the width and height in the <h1> tags
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    widthTag.textContent = "Width: " + width;
+    heightTag.textContent = "Height: " + height;
+}
+
+// Call the function initially to display the initial width and height
+updateSize();
+
+// Add an event listener to the window for the "resize" event
+window.addEventListener("resize", updateSize);
